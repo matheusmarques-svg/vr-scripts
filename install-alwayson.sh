7d61cb19f23ab98c805b86613fe7f568279d125a
matheusmarques-svg <matheus.marques@vrsoft.com.br>
2026-03-09 16:05:01 -0300

    Add files via upload

--- install-alwayson.sh
+++ install-alwayson.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+echo "Script AlwaysOn iniciado"
+
+echo "Instalando dependências..."
+
+sudo apt update
+
+sudo apt install -y \
+libxcb-icccm4 \
+libxcb-image0 \
+libxcb-keysyms1 \
+libxcb-render-util0 \
+libxcb-xinerama0 \
+libxcb-xkb1 \
+libxkbcommon-x11-0
+
+echo "Dependências instaladas com sucesso"
